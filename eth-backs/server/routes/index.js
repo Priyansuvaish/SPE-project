0c60e5c71987911a3019d78572e96bede1f6b6a9
secy2520 <82306041+secy2520@users.noreply.github.com>
2024-05-06 22:38:29 +0530

    Update index.js

--- eth-backs/server/routes/index.js
+++ eth-backs/server/routes/index.js
@@ -3,11 +3,9 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
 
-<<<<<<< HEAD
-//const contractAPIRoutes = require("./contract-API");
-=======
+
 const contractAPIRoutes = require("./contract-API");
->>>>>>> 6eb910aa (updated-eth-back)
+
 const smartContractAPIRoutes = require("./smart-contract-API");
 
 const port = 4000;
@@ -23,25 +21,18 @@ app.use(
 );
 
 // use the routes specified in route folder
-<<<<<<< HEAD
-//app.use("/", contractAPIRoutes);
-=======
+
 app.use("/", contractAPIRoutes);
->>>>>>> 6eb910aa (updated-eth-back)
+
 app.use("/",smartContractAPIRoutes);
 
 app.use(function(err, req,res, next){
     res.status(422).send({error: err.message});
 });
 
-<<<<<<< HEAD
-//listen to the server
-app.listen( port, function(){
-    console.log(`Listening to the port ${port} .....`);
-});
-=======
+
+
 
 app.listen( port, function(){
     console.log(`Listening to the port ${port} .....`);
 });
->>>>>>> 6eb910aa (updated-eth-back)
