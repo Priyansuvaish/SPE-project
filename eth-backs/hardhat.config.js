60759ee61e3c4ca99e4268f28d9979402916ef6c
secy2520 <82306041+secy2520@users.noreply.github.com>
2024-05-12 23:56:10 +0530

    Update hardhat.config.js

--- eth-backs/hardhat.config.js
+++ eth-backs/hardhat.config.js
@@ -1,6 +1,6 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.24",
+  solidity: "0.8.0",
   // networks: {
   //   gtr: {
   //     url: env.PROVIDER_URL,
